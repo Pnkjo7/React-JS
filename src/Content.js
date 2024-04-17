a6ceef90d1e18c0a1b13dd5d0209ea760c501384
Pnkjo7 <pcpankajo7@gmail.com>
2024-04-18 01:20:56 +0530

    React Js forms

--- src/Content.js
+++ src/Content.js
@@ -1,81 +1,21 @@
-import { useState } from "react";
-import { FaTrashAlt} from 'react-icons/fa'
 
+import ItemList from './ItemList'
 
-const Content = () => {
-    
-    const [items, setItems] = useState([
-        {
-            id: 1,
-            checked: true,
-            item: "one half pound bag of cocoa covered almonds unsalted"
-        },
-        {
-            id: 2,
-            checked: false,
-            item: "item2"
-        },
-        {
-            id: 3,
-            checked: false,
-            item: "item3"
-        }
-    ]);
-
-
-    const handleCheck = (id) => {
-        const listItems = items.map((item) => item.id === id ? { ...item, checked: !item.checked } : item );
-        setItems(listItems);
-        localStorage.setItem('shoppinglist', JSON.stringify(listItems));
-
-    }
-
-    const handleDelete = (id) => {
-        const listItems = items.filter((item) => item.id !== id);
-        setItems(listItems);
-        localStorage.setItem('shoppinglist', JSON.stringify(listItems));
-
-    }
-
-  
-    
-    return(
+const Content = ({ items, handleCheck, handleDelete }) => {
+    return (
         <main>
-            { items.length ? (
-                <ul>
-                    {items.map((item) => (
-                        <li className="item" key={item.id}>
+            {items.length ? (
+                <ItemList item={items}
+                    handleCheck={handleCheck}
+                    handleDelete={handleDelete}
 
-                            <input
-                            type="checkbox" 
-                            onChange={() => handleCheck(item.id)}
-                            checked={item.checked}
-                            />
-                            <label 
-                            style={(item.checked)?{textDecoration:'line-through'}:null}
-                            onDoubleClick={() => handleCheck(item.id)}
-                            
-                            >{item.item}</label>
-                            <FaTrashAlt 
-                            onClick={() => handleDelete(item.id)}
-                            role="button" 
-                            tabIndex="0"
-                            />
-                        </li>
-                    ))}
-
-
-                </ul>
-            ):(
-
-                <p style={{marginTop: '2rem'}}>Your list empty.</p>
+                />
+            ) : (
+                <p style={{ marginTop: '2rem' }}>Your list empty.</p>
             )}
-
-
-
-        </main>
+        </main >
     )
-    
+
 }
 
 export default Content
---
+++ src/ItemList.js
@@ -0,0 +1,19 @@
+
+import LineItem from './LineItem';
+
+const ItemList = ({ item, handleCheck, handleDelete }) => {
+    return (
+        <ul>
+            {item.map((item) => (
+                <LineItem
+                    key={item.id}
+                    item={item}
+                    handleCheck={handleCheck}
+                    handleDelete={handleDelete}
+                />
+            ))}
+        </ul>
+    )
+}
+
+export default ItemList
